ca46347d0ed9bc65025cc3c8a217f7eac3d99e0a
eremie523 <reremie523@gmail.com>
2025-11-02 23:27:38 +0100

    Enhance risk scoring functionality: update calculate_risk_score to use token aggregator and add price normalization method

--- shieldfi-protocol/src/risk/stablecoin.sol
+++ shieldfi-protocol/src/risk/stablecoin.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8;
 
 import { Utils } from "./lib/Utils.sol";
+import {AggregatorV3Interface} from "@chainlink/contracts@1.5.0/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 contract STABLECOIN {
     uint256 private constant RISK_FACTOR_TVL = 23;
@@ -10,14 +11,34 @@ contract STABLECOIN {
     uint256 private constant RISK_FACTOR_HISTORICAL_EXPLOIT = 25;
     // uint256 private constant MARKET_VOLATILITY = 
 
-    function calculate_risk_score(address _token) external view returns (uint256) {
-        uint256 tvl_score = (Utils.calculate_tvl_score(_token, 1293) * RISK_FACTOR_TVL) / 100;
-        uint256 protocol_age_score = (Utils.calculate_protocol_age_score(10) * RISK_FACTOR_PROTOCOL_AGE) / 100;
-        uint256 historical_exploit_score = (Utils.calculate_historical_exploit_score(10) * RISK_FACTOR_HISTORICAL_EXPLOIT) / 100;
+    function calculate_risk_score(address _token, address _tokenAggregator) external view returns (uint256) {
+        uint256 tvl_score = Utils.calculate_tvl_score(_token, _getStableCoinPriceToUSDNormalized(_tokenAggregator));
+        // uint256 protocol_age_score = (Utils.calculate_protocol_age_score(10) * RISK_FACTOR_PROTOCOL_AGE) / 100;
+        // uint256 historical_exploit_score = (Utils.calculate_historical_exploit_score(10) * RISK_FACTOR_HISTORICAL_EXPLOIT) / 100;
 
-        uint256 total_score = tvl_score + protocol_age_score + historical_exploit_score;
+        uint256 total_score = tvl_score;
 
         // rationalize to max of 10 and min of 0 and return response
         return total_score / 10;
+    }
+
+    function _getStableCoinPriceToUSDNormalized(address _aggregatorAddress) view public returns (uint256) {
+        AggregatorV3Interface dataFeed = AggregatorV3Interface(_aggregatorAddress);
+
+         // prettier-ignore
+        (
+        /* uint80 roundId */
+        ,
+        int256 answer,
+        /*uint256 startedAt*/
+        ,
+        /*uint256 updatedAt*/
+        ,
+        /*uint80 answeredInRound*/
+        ) = dataFeed.latestRoundData();
+
+        uint8 dec = dataFeed.decimals();
+
+        return uint256(answer) * 1e18 / (10 ** dec);
     }
 }
